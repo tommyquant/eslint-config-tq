fa229b89c215a030d385a2b78eee54f9e5f4d395
Tommy <51040159+tommyquant@users.noreply.github.com>
2020-06-11 21:15:42 +1000

    Separate config files

--- stylelint.js
+++ stylelint.js
@@ -1,14 +1,1 @@
-module.exports = {
-    'plugins': [
-        'stylelint-order'
-    ],
-    'rules': {
-        'color-named': 'never',
-        'color-no-hex': true,
-        'order/order': [
-            'custom-properties',
-            'declarations'
-        ],
-        'order/properties-alphabetical-order': true
-    }
-};
+module.exports = require('./.stylelintrc.json');
